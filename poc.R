280169372410e9aaccb6231c4b0bdb5aea73a651
vbonhomme <bonhomme.vincent@gmail.com>
2016-11-12 22:22:13 +0100

    read_coo on 1+ files

--- poc.R
+++ poc.R
@@ -22,49 +22,51 @@ to_coo.Coo <- function(x){
   f <- function(x) c(x$fac %>% to_coo, x$coo[[1]] %>% to_coo)
   n <- paste0(names(x), ".coo")
   for (i in seq(length(x)))
-    x %>% slice(i) %>% f() %>% write(file=n[i])
+    x %>% slice(i) %>% f() %>% writeLines(con=n[i])
 }
-bot %>% to_coo()
-# bot %>% slice(1) %>% to_coo()
 
 read_coo <- function(file){
-  x <- readLines(file)
-  cov0 <- x %>%
-    grep("^[[:alpha:]]+ [[:alnum:]]+$", ., value=TRUE) %>%
-    strsplit(" ") %>% do.call("rbind", .) %>% t
-  cov <- cov0[-1,, drop=FALSE] %>% data.frame()
-  colnames(cov) <- cov0[1, ]
-
-  p_ids <-  grep("^[[:alpha:]]+[[:alnum:]]*$", x)
-  if (length(p_ids) <= 1){
-    coo_lines <- x %>%
-      grep("^[[:digit:]]+ [[:digit:]]+", ., value=TRUE)
-    coo     <- coo_lines %>%
-      strsplit(" ") %>% lapply(as.numeric) %>%
-      do.call("rbind", .)
+  if (length(file) > 1){
+    nice_names <- file %>% .trim.ext() %>% .trim.path()
+    file %>% lapply(read_coo) %>% `names<-`(nice_names) %>%
+      list(coo=lapply(., `[[`, "coo"),
+           cov=lapply(., `[[`, "cov") %>% do.call("rbind", .)) %>%
+      return()
   } else {
-    p <-  x %>% grep("^[[:digit:]]+ [[:digit:]]+", .)
-    dp <- diff(p)
-    begs <- c(1, p[which(dp!=1)+1])
-    ends <- c(p[which(dp!=1)], tail(p, 1))
-    coo <- vector("list", 3)
-    for (i in seq_along(begs)){
-      coo[[i]] <- coo_lines %>%
-        `[`(begs[i]:ends[i]) %>%
+    x <- readLines(file)
+    cov0 <- x %>%
+      grep("^[[:alpha:]]+ [[:alnum:]]+$", ., value=TRUE) %>%
+      strsplit(" ") %>% do.call("rbind", .) %>% t
+    cov <- cov0[-1,, drop=FALSE] %>% data.frame()
+    colnames(cov) <- cov0[1, ]
+
+    p_ids <-  grep("^[[:alpha:]]+[[:alnum:]]*$", x)
+    if (length(p_ids) <= 1){
+      coo_lines <- x %>%
+        grep("^[[:digit:]]+ [[:digit:]]+", ., value=TRUE)
+      coo     <- coo_lines %>%
         strsplit(" ") %>% lapply(as.numeric) %>%
         do.call("rbind", .)
+    } else {
+      p <-  x %>% grep("^[[:digit:]]+ [[:digit:]]+", .)
+      dp <- diff(p)
+      begs <- c(1, p[which(dp!=1)+1])
+      ends <- c(p[which(dp!=1)], tail(p, 1))
+      coo <- vector("list", 3)
+      for (i in seq_along(begs)){
+        coo[[i]] <- coo_lines %>%
+          `[`(begs[i]:ends[i]) %>%
+          strsplit(" ") %>% lapply(as.numeric) %>%
+          do.call("rbind", .)
+      }
     }
+    list(cov=cov, coo=coo)
   }
-  list(cov=cov, coo=coo)
 }
 
-b1 <- bot %>% slice(1)
-b1 %>% to_coo() %>% write(file="coo_1sttry.coo")
-read_coo("coo_1sttry.coo")
-file.remove("coo_1sttry.coo")
-
+# Benchmarks and examples --------
 library("microbenchmark")
-microbenchmark(b1 %>% to_coo()) #1.4 ms
-microbenchmark(b1 %>% to_coo() %>% write(file="coo_1sttry.coo")) #2.5 ms
-microbenchmark(read_coo("coo_1sttry.coo")) # 1.5 ms
+microbenchmark(bot %>% to_coo(), times=10) # 40 shapes, 164 Ko, 0.6 sec
+microbenchmark(read_coo(list.files()))     # same 0.26 sec
+file.remove(list.files(pattern=".coo"))
 
